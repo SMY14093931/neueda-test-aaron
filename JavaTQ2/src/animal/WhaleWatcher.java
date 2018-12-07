811976c470d8bed98d5552a41ae87f13f7e8ecee
Aaron Smyth (SMY14093931) <SMY14093931@belfastmet.ac.uk>
2018-12-07 15:55:08 +0000

    Week 11 Friday:Test

---
+++ JavaTQ2/src/animal/WhaleWatcher.java
@@ -0,0 +1,23 @@
+package animal;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class WhaleWatcher {
+
+	public static void main(String[] args) {
+		
+		List<Whale> whale = new ArrayList<>();
+		whale.add(new Whale("Right","Atlantic", 2001, 21, 16));
+		whale.add(new Whale("Blue","Pacific", 2001, 23, 16));
+		whale.add(new Whale("Sperm","Atlantic", 1900, 20, 40));
+		whale.add(new Whale("Humpback","Antarctic", 919, 13, 13));
+		
+
+
+	}
+	
+}
