da914b2466b0718345f5e9b75888efc1938a4e3c
Aaron Smyth (SMY14093931) <SMY14093931@belfastmet.ac.uk>
2018-12-07 14:43:52 +0000

    Week 11 Friday:Test

--- JavaTQ2/src/animal/Animal.java
+++ JavaTQ2/src/animal/Animal.java
@@ -1,5 +1,24 @@
 package animal;
 
 public class Animal {
+//	The name property for the class
+	public String name;
+
+//	The constructor
+	public Animal(String name) {
+		super();
+		this.name = name;
+	}
+
+//	The getters and setters of the class
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
